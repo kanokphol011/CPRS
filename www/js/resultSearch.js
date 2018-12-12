4dd40d017d73e810892b9312ca0d1b855dbef682
Janjira012 <s5830213012@phuket.psu.ac.th>
2018-12-12 18:16:46 +0700

    edit

--- www/js/resultSearch.js
+++ www/js/resultSearch.js
@@ -43,9 +43,9 @@ $(function(){
 })
 
 $(function(){
-    var x ='https://api.elsevier.com/content/search/scopus?query=ALL(';
-    var y=')&apiKey=185547eee67ed06e5e817a0f227d23fe';
-    url = x+initials+'%20AND%20'+lastname+y;
+    var x ='https://api.elsevier.com/content/search/scopus?query=AUTHLASTNAME';
+    var y='&apiKey=185547eee67ed06e5e817a0f227d23fe';
+    url = x+'('+ initials +')%20AND%20(' +lastname+')'+y;
     console.log(url);
     xmlhttp.open("GET", url, false);
     xmlhttp.send();
@@ -78,7 +78,7 @@ function myFunction() {
         xmlhttp.send();
         //console.log(DateFrom)
         
-       var url = "reportStaffs.htm?id="+tid+"&name="+name+"&lastname="+lastname+"&year="+yearfrom+"&to="+yearto;
+       var url = "reportStaffs.htm?id="+tid+"&initials="+initials+"&lastname="+lastname+"&year="+yearfrom+"&to="+yearto;
    window.location.assign(url);
     // });
 
